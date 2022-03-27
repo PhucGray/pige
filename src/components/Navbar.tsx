b02e0c4367e7a2cdc5a496a62587ae0c9b999a47
Nguyen Trong Phuc <grayfptcs3@gmail.com>
2022-03-27 21:21:32 +0700

    navbar style

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -52,125 +52,128 @@ const Navbar = () => {
 
   return (
     <div
-      className={`flex items-center justify-between min-h-[57px] z-50 border-b-[1px] ${
+      className={`min-h-[57px] z-50 border-b-[1px] 
+      ${
         isLaptopUp ? 'px-[40px] gap-[20px]' : 'px-[20px]'
       } sticky top-0 bg-white`}>
-      {!isSearching && <Link to='/'>pige</Link>}
-      {isSearching && (
-        <button
-          className='rounded-[10px] text-[24px] h-[45px] flex justify-center items-center w-[45px] hover:bg-slate-100 mr-[3px]'
-          onClick={() => setIsSearching(false)}>
-          <MdArrowBack />
-        </button>
-      )}
-      <form
-        onSubmit={handleSubmit}
-        className={`flex flex-1 h-[45px] md:max-w-[500px] mr-[10px]
+      <div className='max-w-[1200px] mx-auto h-full flex items-center justify-between'>
+        {!isSearching && <Link to='/'>pige</Link>}
+        {isSearching && (
+          <button
+            className='rounded-[10px] text-[24px] h-[45px] flex justify-center items-center w-[45px] hover:bg-slate-100 mr-[3px]'
+            onClick={() => setIsSearching(false)}>
+            <MdArrowBack />
+          </button>
+        )}
+        <form
+          onSubmit={handleSubmit}
+          className={`flex flex-1 h-[45px] md:max-w-[500px] mr-[10px]
             ${
               isSearching || isLaptopUp
                 ? 'border-[1px] justify-between'
                 : 'border-none justify-end'
             }
             `}>
-        {(isSearching || isLaptopUp) && (
-          <input
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-            type='text'
-            placeholder='Nhập từ khoá'
-            className='flex-1 form-control'
-            autoFocus={isSearching}
-          />
-        )}
-
-        <button
-          className={`w-[45px] text-[24px] ${
-            isSearching || isLaptopUp
-              ? 'bg-primary text-white ring-primary ring-1'
-              : 'hover:bg-slate-100 rounded-[10px]'
-          }`}
-          onClick={() => {
-            if (isLaptopUp) return;
-            setIsSearching(true);
-          }}>
-          <BiSearch className='mx-auto' />
-        </button>
-      </form>
-
-      {userLoading && <>.</>}
-
-      {!isSearching && !userLoading && (
-        <>
-          {!user && (
-            <Link to='/sign-in' className='cursor-pointer hover:text-primary'>
-              Đăng nhập/ Đăng ký
-            </Link>
+          {(isSearching || isLaptopUp) && (
+            <input
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              type='text'
+              placeholder='Nhập từ khoá'
+              className='flex-1 form-control'
+              autoFocus={isSearching}
+            />
           )}
 
-          {user && (
-            <>
-              <div className='flex items-center gap-[10px]'>
-                <button
-                  title='Bài viết đã lưu'
-                  onClick={() => navigate('/bookmark')}
-                  className='rounded-[10px] w-[45px] h-[45px] flex justify-center items-center hover:bg-slate-100'>
-                  <BsBookmark size={25} />
-                </button>
-                <button
-                  title='Viết bài'
-                  onClick={() => {
-                    dispatch(setCurrentPost(null));
-                    navigate('/new-post');
-                  }}
-                  className='rounded-[10px] w-[45px] h-[45px] flex justify-center items-center hover:bg-slate-100'>
-                  <BiEdit size={30} />
-                </button>
+          <button
+            className={`w-[45px] text-[24px] ${
+              isSearching || isLaptopUp
+                ? 'bg-primary text-white ring-primary ring-1'
+                : 'hover:bg-slate-100 rounded-[10px]'
+            }`}
+            onClick={() => {
+              if (isLaptopUp) return;
+              setIsSearching(true);
+            }}>
+            <BiSearch className='mx-auto' />
+          </button>
+        </form>
 
-                <div className='relative'>
-                  <img
-                    src={user.photoURL || '/default-avatar.jpg'}
-                    alt='avatar'
-                    className='rounded-full cursor-pointer'
-                    height={40}
-                    width={40}
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      setDropdown(!dropdown);
+        {userLoading && <>.</>}
+
+        {!isSearching && !userLoading && (
+          <>
+            {!user && (
+              <Link to='/sign-in' className='cursor-pointer hover:text-primary'>
+                Đăng nhập/ Đăng ký
+              </Link>
+            )}
+
+            {user && (
+              <>
+                <div className='flex items-center gap-[10px]'>
+                  <button
+                    title='Bài viết đã lưu'
+                    onClick={() => navigate('/bookmark')}
+                    className='rounded-[10px] w-[45px] h-[45px] flex justify-center items-center hover:bg-slate-100'>
+                    <BsBookmark size={25} />
+                  </button>
+                  <button
+                    title='Viết bài'
+                    onClick={() => {
+                      dispatch(setCurrentPost(null));
+                      navigate('/new-post');
                     }}
-                  />
+                    className='rounded-[10px] w-[45px] h-[45px] flex justify-center items-center hover:bg-slate-100'>
+                    <BiEdit size={30} />
+                  </button>
 
-                  {dropdown && (
-                    <>
-                      <div className='absolute min-w-[240px] right-0 border bg-white py-[5px] rounded-lg'>
-                        <Link
-                          to='/my-posts'
-                          className='px-[20px] flex items-center space-x-[5px] text-[18px] h-[40px] cursor-pointer hover:bg-slate-100'>
-                          <span className='min-w-[30px]'>
-                            <CgFileDocument />
-                          </span>
-                          <span>Bài viết của bạn</span>
-                        </Link>
-                        <div
-                          onClick={async () => {
-                            await signOut(auth);
-                            navigate('/sign-in');
-                            dispatch(setUser(null));
-                          }}
-                          className='px-[20px] flex items-center space-x-[5px] text-[18px] h-[40px] cursor-pointer hover:bg-slate-100'>
-                          <span className='min-w-[30px]'>
-                            <MdOutlineLogout />
-                          </span>
-                          <span>Đăng xuất</span>
+                  <div className='relative'>
+                    <img
+                      src={user.photoURL || '/default-avatar.jpg'}
+                      alt='avatar'
+                      className='rounded-full cursor-pointer'
+                      height={40}
+                      width={40}
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        setDropdown(!dropdown);
+                      }}
+                    />
+
+                    {dropdown && (
+                      <>
+                        <div className='absolute min-w-[240px] right-0 border bg-white py-[5px] rounded-lg'>
+                          <Link
+                            to='/my-posts'
+                            className='px-[20px] flex items-center space-x-[5px] text-[18px] h-[40px] cursor-pointer hover:bg-slate-100'>
+                            <span className='min-w-[30px]'>
+                              <CgFileDocument />
+                            </span>
+                            <span>Bài viết của bạn</span>
+                          </Link>
+                          <div
+                            onClick={async () => {
+                              await signOut(auth);
+                              navigate('/sign-in');
+                              dispatch(setUser(null));
+                            }}
+                            className='px-[20px] flex items-center space-x-[5px] text-[18px] h-[40px] cursor-pointer hover:bg-slate-100'>
+                            <span className='min-w-[30px]'>
+                              <MdOutlineLogout />
+                            </span>
+                            <span>Đăng xuất</span>
+                          </div>
                         </div>
-                      </div>
-                    </>
-                  )}
+                      </>
+                    )}
+                  </div>
                 </div>
-              </div>
-            </>
-          )}
-        </>
-      )}
+              </>
+            )}
+          </>
+        )}
+      </div>
     </div>
   );
 };
